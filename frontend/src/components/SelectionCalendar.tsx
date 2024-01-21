f4ef43ec9f966ea140ffe39bed7369bd2224704e
shouvik <shouvikdeebee@gmail.com>
2024-01-21 09:20:44 -0500

    Revert "Revert "month wow""
    This reverts commit 021c23bae872c30fab55ea112749b06aea788332.

--- frontend/src/components/SelectionCalendar.tsx
+++ frontend/src/components/SelectionCalendar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './SelectionCalendar.css'; // Assuming you'll create this CSS file
 
 interface CalendarProps {
   onSelectDate: (date: Date) => void;
@@ -17,10 +18,11 @@ const SelectionCalendar: React.FC<CalendarProps> = ({ onSelectDate }) => {
   };
 
   return (
-    <div>
+    <div className="calendar-container">
       <Calendar
         onChange={handleDateChange as any}
         value={selectedDate instanceof Date ? selectedDate : undefined}
+        calendarType="US"
       />
     </div>
   );
